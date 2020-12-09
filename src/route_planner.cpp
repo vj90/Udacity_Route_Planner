f65220cda306e1c0ed4d79541fd4c74813a57507
Vasundhara Jain <vasundhara@flex.fritz.box>
2020-12-09 16:02:48 +0100

    TODO 6: Complete the ConstructFinalPath method

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -43,14 +43,6 @@ RouteModel::Node *RoutePlanner::NextNode() {
 }
 
 
-// TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
-// Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
-
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     this->distance = 0.0f;
